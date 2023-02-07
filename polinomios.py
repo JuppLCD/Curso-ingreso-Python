97cb4fc306a8ab1e3ecd1f19eb715b9b3b703ec5
JuppLCD <lorenzocanovas207@gmail.com>
2023-02-07 14:26:21 -0300

    Add package autopep8

--- polinomios.py
+++ polinomios.py
@@ -11,33 +11,37 @@ La forma de colocar coeficientes es desde el polinomio de menor orden al mayor,
 es decir, si tengo un polinomio [X^2 + 3X + 5] ==> la lista de coeficientes es [5, 3, 1]
 '''
 
+
 class Polinomio:
     def __init__(self) -> None:
         pass
 
     @staticmethod
-    def suma( *listas_coeficientes):
-        polinomios = Polinomio.listas_coeficientes_a_polinomios(listas_coeficientes)
-        resultado = reduce(lambda x,y: x+y, polinomios)
+    def suma(*listas_coeficientes):
+        polinomios = Polinomio.listas_coeficientes_a_polinomios(
+            listas_coeficientes)
+        resultado = reduce(lambda x, y: x+y, polinomios)
 
         Polinomio.print_resultado(resultado)
 
     @staticmethod
-    def resta( *listas_coeficientes):
-        polinomios = Polinomio.listas_coeficientes_a_polinomios(listas_coeficientes)
-        resultado = reduce(lambda x,y: x-y, polinomios)
+    def resta(*listas_coeficientes):
+        polinomios = Polinomio.listas_coeficientes_a_polinomios(
+            listas_coeficientes)
+        resultado = reduce(lambda x, y: x-y, polinomios)
 
         Polinomio.print_resultado(resultado)
 
     @staticmethod
-    def multiplicacion( *listas_coeficientes):
-        polinomios = Polinomio.listas_coeficientes_a_polinomios(listas_coeficientes)
-        resultado = reduce(lambda x,y: x*y, polinomios)
+    def multiplicacion(*listas_coeficientes):
+        polinomios = Polinomio.listas_coeficientes_a_polinomios(
+            listas_coeficientes)
+        resultado = reduce(lambda x, y: x*y, polinomios)
 
         Polinomio.print_resultado(resultado)
 
     @staticmethod
-    def divicion( polinomio_numerador, polinomio_denominador) :
+    def divicion(polinomio_numerador, polinomio_denominador):
         nominador = Polynomial(coef=polinomio_numerador)
         denominador = Polynomial(coef=polinomio_denominador)
 
@@ -47,8 +51,8 @@ class Polinomio:
         resultado = f'P(x) = Q(x) . D(x) + R(x) ===> P(x) = ( {cociente.convert()} ) . ( {denominador.convert()} ) + ( {resto.convert()} )'
         print(resultado)
 
-    def print_resultado( resultado):
-        print( f'{resultado.convert()}' )
+    def print_resultado(resultado):
+        print(f'{resultado.convert()}')
 
     def listas_coeficientes_a_polinomios(listas_coeficientes):
-        return list(map(lambda coeficientes :Polynomial(coef=coeficientes) ,listas_coeficientes))
+        return list(map(lambda coeficientes: Polynomial(coef=coeficientes), listas_coeficientes))
